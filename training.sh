02de0da01c2ebd2d9822964b9f3261552166f962
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2025-08-18 17:36:41 +0200

    Exp1 - Evaluation parallelisation + prepare the job in training.sh

--- training.sh
+++ training.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=general --qos=medium
-#SBATCH --time=16:00:00
-#SBATCH --mincpus=2
-#SBATCH --mem=20000
+#SBATCH --time=8:00:00
+#SBATCH --mincpus=8
+#SBATCH --mem=40000
 #SBATCH --gres=gpu:1
 
-#SBATCH --job-name=trainpfn
+#SBATCH --job-name=parallel_evaluation_exp1
 #SBATCH --output=out_trainpfn_%A_%a.txt
 #SBATCH --error=err_trainpfn_%A_%a.txt
 #SBATCH --array=0-0
@@ -26,6 +26,6 @@ export PYTHONUNBUFFERED=TRUE
 export PYTHONPATH="${PYTHONPATH}:${PWD}"
 
 cd /tudelft.net/staff-umbrella/lcdb2/adelina/Extrapolating-Learning-Curves-When-Do-Neural-Networks-Outperform-Parametric-Models-
-srun python experiment1/training-pfn-lcdb11-experiment1.py --seed $SLURM_ARRAY_TASK_ID
+srun python experiment1/evaluation_parametric_vs_lcpfn.py --seed $SLURM_ARRAY_TASK_ID
 
 conda deactivate
