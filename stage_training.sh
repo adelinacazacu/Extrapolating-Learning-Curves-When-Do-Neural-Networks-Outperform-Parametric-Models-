89a519acdf81ac4bd4df108028201b2f407f9803
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2025-09-05 01:25:58 +0300

    Request a40 instead

--- stage_training.sh
+++ stage_training.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=1:00:00
 #SBATCH --mincpus=2
 #SBATCH --mem=4GB
-#SBATCH --gres=gpu:l40:1
+#SBATCH --gres=gpu:a40:1
 
 #SBATCH --job-name=pfn_stage_%A
 #SBATCH --output=out_stage_%A.txt
