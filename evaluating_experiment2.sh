6966dcbc602ab7c41e9babbb2a988b9725b0c455
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2025-09-10 21:55:41 +0200

    req gpu14

--- evaluating_experiment2.sh
+++ evaluating_experiment2.sh
@@ -3,8 +3,7 @@
 #SBATCH --time=4:00:00
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=16GB
-#SBATCH --nodes=1
-#SBATCH --nodelist=influ[4-6],gpu[14-35]
+#SBATCH --nodelist=gpu14
 
 #SBATCH --job-name=paralleleval
 #SBATCH --output=out_paralleleval_%A_%a.txt
